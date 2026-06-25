67d062e753b40c0233e0bacbdbf07072834628c3
Test User <test@example.com>
2026-06-25 15:48:16 +0530

    refactor logipage constructor

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -6,21 +6,22 @@ import { LoginPageLocators } from '@locators/LoginPageLocators';
 
 export class LoginPage {
 
-    readonly username;
-    readonly password;
-    readonly loginBtn;
+    // readonly username;
+    // readonly password;
+    // readonly loginBtn;
 
     constructor(private page: Page) {
-        this.username = page.getByPlaceholder(LoginPageLocators.username)
-        this.password = page.getByPlaceholder(LoginPageLocators.password);
-        this.loginBtn = page.getByRole("button",{name:LoginPageLocators.loginBtn})
+        // this.username = page.getByPlaceholder(LoginPageLocators.username)
+        // this.password = page.getByPlaceholder(LoginPageLocators.password);
+        // this.loginBtn = page.getByRole("button",{name:LoginPageLocators.loginBtn})
+        this.page = page;
     }
 
     async login(user: string, pass: string) {
 
         await this.page.goto(Env.baseUrl)
-        await this.username.fill(user);
-        await this.password.fill(pass);
-        await this.loginBtn.click();
+        await this.page.getByPlaceholder(LoginPageLocators.username).fill(user)
+        await this.page.getByPlaceholder(LoginPageLocators.password).fill(pass)
+        await this.page.getByRole("button", { name: LoginPageLocators.loginBtn }).click()
     }
 }
