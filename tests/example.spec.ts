f6055e6eab31889fdcb516c823e80f0a7e801764
Test User <test@example.com>
2026-06-25 10:17:45 +0530

    github actions yml added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@fixtures/PageFixture'
 
 
-test("demo", async ({ pages }) => {
+test("@smoke demo", async ({ pages }) => {
 
     await pages.getLoginPage().login()
     await pages.getDashboardPage().verifydashBoard()
